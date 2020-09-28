035dbfca06360215d757ef7558ec3353d9af067b
dinesh-k-natarajan <dineshkrishna.natarajan@gmail.com>
2020-09-28 14:12:28 +0200

    clean up using gitignore

--- copytopublicrepo.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-require 'fileutils'
-include FileUtils
-
-source_directory = "C:/Users/dines/Documents/GitHub/githubpages-sourcecode/_site"
-destination_directory = "C:/Users/dines/Documents/GitHub/dinesh-k-natarajan.github.io/"
-
-FileUtils.copy_entry(source_directory, destination_directory, remove_destination = true)
